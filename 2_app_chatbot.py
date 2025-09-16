44f1602903777e2044b7d219a72aaaaefb7864cc
Miguel Riano <795976+trunimrod@users.noreply.github.com>
2025-09-15 21:04:14 -0500

    Mejoras a la UI con medicon de tokens

--- 2_app_chatbot.py
+++ 2_app_chatbot.py
@@ -86,9 +86,9 @@ MODELO_EMBEDDING = os.environ.get("EMBED_MODEL", "nomic-embed-text")
 MODELO_LLM = os.environ.get("LLM_MODEL", "llama3.2")
 NOMBRE_COLECCION = "sentencias_anla" 
 
-# Parámetros MMR simples
-K_DOCUMENTOS = 4
-FETCH_K = 20
+# Parámetros MMR mejorados para una búsqueda más exhaustiva
+K_DOCUMENTOS = 8
+FETCH_K = 40
 MMR_LAMBDA = 0.5
 
 # Contexto
